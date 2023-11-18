ffd806956039846aa27b8a91f8fc11f8d9837429
Rehtest <rehtesten@gmail.com>
2023-11-18 19:03:36 +0530

    Split code into components

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,9 @@
+export default function CoreConcept({image, title, description}) { // Can also destructure props in the function signature
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+}
