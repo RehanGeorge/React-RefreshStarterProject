3db4976bec0fbdfb0e66980b78227997d3be764e
RehanGeorge <rehan.george@gmail.com>
2023-11-20 19:35:34 +0530

    Split App into Components

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,13 @@
+import CoreConcept from "./CoreConcept/CoreConcept";
+import Section from "./Section";
+import { CORE_CONCEPTS } from "../data";
+
+export default function CoreConcepts() {
+    return (
+        <Section id="core-concepts" title="Core Concepts">
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+          </ul>
+        </Section>
+    );
+}
